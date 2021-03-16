2ef537ffe57f7d531115d1474116b170ad914738
medium0fry <medium0fry@gmail.com>
2021-03-16 15:03:26 -0400

    Started adding vector calculation

--- js/main.js
+++ js/main.js
@@ -1,14 +1,61 @@
 
+
+
 // document.addEventListener('keydown',)
 
 let locationData = {}
 
 class Location {
-   constructor(name) {
-      this.name = name
-      this.distance = 0
-      this.azimuth = 0
+   constructor(d = 1, a = 0) {
+      this.distance = d
+      this.azimuth = a
    }
+
+   equals(rhs) {
+      return (this.distance === rhs.distance
+         && this.azimuth === rhs.azimuth)
+   }
+}
+
+class Side {
+   constructor(l = 0) {
+      this.length = l
+   }
+}
+
+class Angle {
+   constructor(a = 0) {
+      this._radians = a
+      this.degrees = Angle.toDegrees(a)
+   }
+
+   get radians() {
+      return this._radians
+   }
+
+   set radians(r) {
+      this._radians = r
+      this.degrees = Angle.toDegrees(this._radians)
+   }
+
+   validate() {
+      let degrees = this.degrees % 360
+      this.radians = Angle.toRadians(degrees)
+   }
+
+   static toRadians(degrees) {
+      return degrees * Math.PI / 180
+   }
+   static toDegrees(radians) {
+      return radians * 180 / Math.PI
+   }
+
+   // Since JS doesn't have operator overloading for objects
+   negate = () => new Angle(-this.radians)
+   subtract = (rhs) => new Angle(this.radians - rhs.radians)
+   add = (rhs) => new Angle(this.radians + rhs.radians)
+   lessThan = (rhs) => this.radians < rhs.radians
+   greaterThan = (rhs) => this.radians > rhs.radians
 
 }
 
@@ -28,7 +75,7 @@ function initButtonList(btn_list) {
    btn_list.forEach(element => {
       element.addEventListener('click', selectLocation)
       element.addEventListener('dblclick', renameButton)
-      locationData[element.name] = new Location(element.value)
+      locationData[element.name] = new Location()
    });
    loadLocation(btn_list[0])
 }
@@ -36,6 +83,7 @@ function initButtonList(btn_list) {
 function initInputList(inputs) {
    inputs.forEach(element => {
       element.addEventListener('change', saveLocation)
+      element.addEventListener('change', calcVector)
    })
 }
 
@@ -77,6 +125,81 @@ function saveLocation(changed) {
    if (selected) {
       locationData[selected.name][attribute] = Number(value)
    }
+}
+
+function calcVector() {
+   let key_t = document.querySelector('#target .btn.selected').name
+   let key_f = document.querySelector('#friend .btn.selected').name
+
+   let target = locationData[key_t]
+   let friend = locationData[key_f]
+   let result = calc(friend, target)
+}
+
+function calc(friend, target) {
+   if (friend.equals(target)) {
+      return new Location(0, 0)
+   }
+
+   function lawOfCosinesTwoSides(a, b, theta) {
+      let c = new Side()
+      let a_2 = a.length ** 2
+      let b_2 = b.length ** 2
+      let cosine = 2 * a.length * b.length * Math.cos(theta.radians)
+
+      let c_2 = a_2 + b_2 - cosine
+      c.length = Math.sqrt(c_2)
+      return c
+   }
+
+   function lawOfCosinesThreeSides(a, b, c) {
+      let a_2 = a.length ** 2
+      let b_2 = b.length ** 2
+      let c_2 = c.length ** 2
+      let divisor = 2 * a.length * b.length
+
+      let cosC = (a_2 + b_2 - c_2) / divisor
+      return new Angle(Math.acos(cosC))
+   }
+
+
+   const SF = new Side(friend.distance) // Spotter to Friend
+   const ST = new Side(target.distance) // Spotter to Target
+   let FT                               // Friend  to Target (result)
+
+   const NSF = new Angle(Angle.toRadians(friend.azimuth)) // North to Spotter to Friend
+   const NST = new Angle(Angle.toRadians(target.azimuth)) // North to Spotter to Target
+   let NFT                                                // North to Friend  to Target (result)
+
+   // Calculated values
+   let SFT                       // Spotter to Friend to Target
+   let delta                     // Change between NSF and NST
+   const PI = new Angle(Math.PI) // 180 degree angle for easy computing
+
+   delta = new Angle(Math.abs(NSF.radians - NST.radians))
+   FT = lawOfCosinesTwoSides(SF, ST, delta)
+   // Order matters here. Side ST must always be the 3rd parameter.
+   SFT = lawOfCosinesThreeSides(SF, FT, ST)
+   console.log(SF.length, FT.length, ST.length, SFT.radians)
+   SFT_degrees = Math.round(SFT.degrees)
+
+   // Make sure Angle stays between 0 - 360 degrees or 0 and 2PI radians
+   delta.validate()
+   //NFT = NSF + PI + ((delta.degrees > PI.degrees ^ NST > NSF) ? -SFT : SFT)
+
+   NFT = new Angle(
+      NSF.radians
+         + PI.radians
+         + (delta.degrees > PI.degrees ^ NST.radians > NSF.radians)
+         ? (-SFT.radians)
+         : SFT.radians
+   )
+   console.log(NFT)
+   NFT.validate()
+
+
+   return new Location(FT.length, Angle.toDegrees(NFT.radians))
+
 }
 
 init()
