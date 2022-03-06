83fd16bd12fefd200b1ee28e58a4a57b6e9a02aa
medium0fry <medium0fry@gmail.com>
2022-03-06 13:53:11 -0500

    Fixed version incompatibilities for bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
 	<div id="app">
 		<img alt="Vue logo" src="./assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<button-group :numButtons="6" />
 	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import ButtonGroup from './components/buttonGroup/ButtonGroup';
 
 export default {
 	name: 'App',
 	components: {
-		HelloWorld,
+		ButtonGroup,
 	},
 };
 </script>
---
+++ src/components/buttonGroup/ButtonGroup.vue
@@ -0,0 +1,16 @@
+<template>
+	<section>
+		<b-button v-for="button in numButtons" :key="button">
+			Location {{ button }}
+		</b-button>
+	</section>
+</template>
+<script>
+export default {
+	name: 'ButtonGroup',
+	props: {
+		numButtons: Number,
+	},
+};
+</script>
+<style scoped></style>
